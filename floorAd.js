37815ddab3b7c893564e7c9c7c24a2ef500ac018
Lada-aym <59732927+Lada-aym@users.noreply.github.com>
2025-02-20 13:10:50 +0400

    Add files via upload

---
+++ floorAd.js
@@ -0,0 +1,10 @@
+window.yaContextCb = window.yaContextCb || ;
+window.yaContextCb.push(() => {
+ Ya.adfoxCode.create({
+ ownerId: 322697,
+ containerId: 'adfox_174003288637644357',
+ type: 'floorAd',
+ platform: 'touch',
+ params: { p1: 'cdvyr', p2: 'ghnb' },
+ });
+});
