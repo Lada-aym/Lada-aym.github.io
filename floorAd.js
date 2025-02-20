e0b8403a40690644ef0f2c65ec66c0021f3cc50c
Lada-aym <59732927+Lada-aym@users.noreply.github.com>
2025-02-20 13:31:22 +0400

    Update floorAd.js

--- floorAd.js
+++ floorAd.js
@@ -9,3 +9,22 @@ window.yaContextCb.push(()=>{
             }
         })
     })
+<!--AdFox START-->
+<!--yandex_acer.manuilov-->
+<!--Площадка: lada-aym.github.io / * / *-->
+<!--Тип баннера: FlorAd-->
+<!--Расположение: низ страницы-->
+<div id="adfox_174004375205214357"></div>
+<script>
+    window.yaContextCb.push(()=>{
+        Ya.adfoxCode.create({
+            ownerId: 322697,
+            containerId: 'adfox_174004375205214357',
+            params: {
+                pp: 'i',
+                ps: 'iwjk',
+                p2: 'hiuq'
+            }
+        })
+    })
+</script>
