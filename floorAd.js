b8ebe81ab9a671986cdab13855d030e69fa1a765
Lada-aym <59732927+Lada-aym@users.noreply.github.com>
2025-02-20 13:25:08 +0400

    Update floorAd.js

--- floorAd.js
+++ floorAd.js
@@ -1,10 +1,11 @@
-window.yaContextCb = window.yaContextCb || ;
-window.yaContextCb.push(() => {
- Ya.adfoxCode.create({
- ownerId: 322697,
- containerId: 'adfox_174003288637644357',
- type: 'floorAd',
- platform: 'touch',
- params: { p1: 'cdvyr', p2: 'ghnb' },
- });
-});
+window.yaContextCb.push(()=>{
+        Ya.adfoxCode.create({
+            ownerId: 322697,
+            containerId: 'adfox_174003288637644357',
+            params: {
+                pp: 'i',
+                ps: 'iwji',
+                p2: 'hiuq'
+            }
+        })
+    })
