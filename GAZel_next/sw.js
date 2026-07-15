bed025e7186ec50f105699a8af3a78afe4b65be8
Lada-aym <59732927+Lada-aym@users.noreply.github.com>
2026-07-15 14:55:00 +0400

    Update sw.js

--- GAZel_next/sw.js
+++ GAZel_next/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v41-2026-07-14-cwv-optimization";
+const CACHE_VERSION = "v42-2026-07-14-cwv-optimization";
 const CACHE_PREFIX = "gazelle-next-manual-";
 const CACHE_NAME = `${CACHE_PREFIX}${CACHE_VERSION}`;
 
